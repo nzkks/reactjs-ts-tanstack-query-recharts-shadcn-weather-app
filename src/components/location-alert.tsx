ab7314b70c32cf70e2760e37358541bac58abc43
nzkks <kkshanthosh@gmail.com>
2025-03-23 10:54:49 +1300

    Weather Dashboard page: Add a refresh button to fetch the location and handle the error and isLoading and no coordinates issue

---
+++ src/components/location-alert.tsx
@@ -0,0 +1,33 @@
+import { Alert, AlertDescription, AlertTitle } from './ui/alert';
+import { Button } from './ui/button';
+import { AlertTriangle, MapPin } from 'lucide-react';
+
+type LocationAlert = {
+  variant?: 'default' | 'destructive';
+  title: string;
+  description: string;
+  btnText: string;
+  btnFunction: () => void;
+};
+
+export default function LocationAlert({
+  variant = 'default',
+  title,
+  description,
+  btnText,
+  btnFunction,
+}: LocationAlert) {
+  return (
+    <Alert variant={variant}>
+      <AlertTriangle className="h-4 w-4" />
+      <AlertTitle>{title}</AlertTitle>
+      <AlertDescription className="flex flex-col gap-4">
+        <p>{description}</p>
+        <Button variant="outline" onClick={btnFunction} className="w-fit">
+          <MapPin className="mr-2 h-4 w-4" />
+          {btnText}
+        </Button>
+      </AlertDescription>
+    </Alert>
+  );
+}
--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,3 +1,64 @@
+import { Button } from '@/components/ui/button';
+import { RefreshCw } from 'lucide-react';
+
+import { useGeolocation } from '@/hooks/use-geolocation';
+import WeatherSkeleton from '@/components/loading-skeleton';
+import LocationAlert from '@/components/location-alert';
+
 export default function WeatherDashboard() {
-  return <div>Weather</div>;
+  const { coordinates, error: locationError, isLoading: locationLoading, getLocation } = useGeolocation();
+
+  const handleRefresh = () => {
+    getLocation();
+
+    if (coordinates) {
+      // reload weather data
+    }
+  };
+
+  if (locationLoading) {
+    return <WeatherSkeleton />;
+  }
+
+  if (locationError) {
+    return (
+      <LocationAlert
+        variant="destructive"
+        title="Location Error"
+        description={locationError}
+        btnText="Enable Location"
+        btnFunction={getLocation}
+      />
+    );
+  }
+
+  if (!coordinates) {
+    return (
+      <LocationAlert
+        title="Location Required"
+        description="Please enable location access to see your local weather."
+        btnText="Enable Location"
+        btnFunction={getLocation}
+      />
+    );
+  }
+
+  return (
+    <div className="space-y-4">
+      {/* Favorite cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-fold tracking-tight">My Location</h1>
+        <Button
+          variant={'outline'}
+          size={'icon'}
+          onClick={handleRefresh}
+          // disabled={}
+        >
+          <RefreshCw className="h-4 w-4" />
+        </Button>
+      </div>
+
+      {/* Current and Hourly weather */}
+    </div>
+  );
 }
