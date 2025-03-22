3b37f61ee14b9e1990ff62b42b1b119b8be12de7
nzkks <kkshanthosh@gmail.com>
2025-03-22 19:20:01 +1300

    install and configure Tanstack Query and its devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { ThemeProvider } from '@/components/theme-provider';
 import { Layout } from '@/components/layout';
@@ -6,17 +8,21 @@ import WeatherDashboard from '@/pages/weather-dashboard';
 import CityPage from '@/pages/city-page';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<WeatherDashboard />} />
-            <Route path="/city/:cityName" element={<CityPage />} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
